7f72a51ea7a1aef876b0953c07f3af1ee180eed9
Ton <ton.paes.bccs@gmail.com>
2019-03-28 22:29:56 -0300

    neural net example

---
+++ main.py
@@ -0,0 +1,16 @@
+import tensorflow.keras as keras
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import deep
+
+new_model = tf.keras.models.load_model('num_reader.model')
+
+predictions = new_model.predict(deep.x_test)
+
+while 1:
+    number = int(input())
+    print(np.argmax(predictions[number]))
+
+    plt.imshow(deep.x_test[number], cmap=plt.cm.binary)
+    plt.show()
