e64a1b87aa63c57de8ba1614970a70e7e74c6461
Ton <ton.paes.bccs@gmail.com>
2019-03-31 11:46:53 -0300

    Changes on deep,py and minor tweaks

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import tensorflow.keras as keras
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 import deep
+import random
+
 
 new_model = tf.keras.models.load_model('num_reader.model')
 
 predictions = new_model.predict(deep.x_test)
 
 while 1:
-    number = int(input())
+    input()
+    number = random.randint(0, 1000)
     print(np.argmax(predictions[number]))
 
     plt.imshow(deep.x_test[number], cmap=plt.cm.binary)
     plt.show()
-# comment to x_test
